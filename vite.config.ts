92e7f49adb13e58ddde124b0b898d8271d0a56cd
tunapahm2xx3 <niyanime282@gmail.com>
2024-12-26 15:01:47 +0700

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import taildwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
   css: {
-    postcss: {
-      plugins: [taildwindcss]
-    }
+    devSourcemap: true
   },
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
